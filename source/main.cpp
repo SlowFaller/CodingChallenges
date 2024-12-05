fc3569de3cc4506db1732207db720f374179dc46
Richard Ortiz <SlowFaller@users.noreply.github.com>
2024-12-04 19:29:33 -0800

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -17,12 +17,12 @@ int main()
 
     RUtility::PalindromeChecker pc;
 
-    cout << "Testing " << to_string(testNumber1) << ", Is Palindrome: " << pc.CheckPalindrome(testNumber1) << endl;
-    cout << "Testing " << to_string(testNumber2) << ", Is Palindrome: " << pc.CheckPalindrome(testNumber2) << endl;
-    cout << "Testing " << to_string(testNumber3) << ", Is Palindrome: " << pc.CheckPalindrome(testNumber3) << endl;
-    cout << "Testing " << testString1 << ", Is Palindrome: " << pc.CheckPalindrome(testString1) << endl;
-    cout << "Testing " << testString2 << ", Is Palindrome: " << pc.CheckPalindrome(testString2) << endl;
-    cout << "Testing " << testString3 << ", Is Palindrome: " << pc.CheckPalindrome(testString3) << endl;
+    cout << "Testing " << to_string(testNumber1) << ", Is Palindrome: " << pc.IsPalindrome(testNumber1) << endl;
+    cout << "Testing " << to_string(testNumber2) << ", Is Palindrome: " << pc.IsPalindrome(testNumber2) << endl;
+    cout << "Testing " << to_string(testNumber3) << ", Is Palindrome: " << pc.IsPalindrome(testNumber3) << endl;
+    cout << "Testing " << testString1 << ", Is Palindrome: " << pc.IsPalindrome(testString1) << endl;
+    cout << "Testing " << testString2 << ", Is Palindrome: " << pc.IsPalindrome(testString2) << endl;
+    cout << "Testing " << testString3 << ", Is Palindrome: " << pc.IsPalindrome(testString3) << endl;
     
     return 0;
 }
